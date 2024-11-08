6c953a24f944965342f689e614207b856fdbb34b
pittsy <p.czyganov@mail.ru>
2024-11-08 19:30:47 +0300

    clang-format

--- src/bimap_iterator.h
+++ src/bimap_iterator.h
@@ -5,85 +5,84 @@
 #include <iterator>
 #include <utility>
 
-template<typename BimapNode, typename Node, typename FlipNode>
+template <typename BimapNode, typename Node, typename FlipNode>
 class bimap_iterator {
 public:
-    using value_type = typename Node::value_type;
-    using tag = typename Node::tag;
-    using pointer = const value_type *;
-    using reference = const value_type &;
-    using difference_type = std::ptrdiff_t;
-    using iterator_category = std::bidirectional_iterator_tag;
+  using value_type = typename Node::value_type;
+  using tag = typename Node::tag;
+  using pointer = const value_type*;
+  using reference = const value_type&;
+  using difference_type = std::ptrdiff_t;
+  using iterator_category = std::bidirectional_iterator_tag;
 
-    bimap_iterator() = default;
+  bimap_iterator() = default;
 
-    reference operator*() const {
-        return static_cast<Node const *>(ptr_)->data;
+  reference operator*() const {
+    return static_cast<Node const*>(ptr_)->data;
+  }
+
+  pointer operator->() const {
+    return &static_cast<Node const*>(ptr_)->data;
+  }
+
+  bimap_iterator& operator++() {
+    ptr_ = ptr_->next();
+    return *this;
+  }
+
+  bimap_iterator operator++(int) {
+    bimap_iterator tmp = *this;
+    ++*this;
+    return tmp;
+  }
+
+  bimap_iterator& operator--() {
+    ptr_ = ptr_->prev();
+    return *this;
+  }
+
+  bimap_iterator operator--(int) {
+    bimap_iterator tmp = *this;
+    --*this;
+    return tmp;
+  }
+
+  bool operator==(const bimap_iterator& other) const {
+    return ptr_ == other.ptr_;
+  }
+
+  bool operator!=(const bimap_iterator& other) const {
+    return !(*this == other);
+  }
+
+  bimap_iterator<BimapNode, FlipNode, Node> flip() const {
+    if (is_valid_node(ptr_)) {
+      return static_cast<const FlipNode*>(static_cast<const BimapNode*>(static_cast<const Node*>(ptr_)));
+    } else {
+      return static_cast<const node_sentinel<typename FlipNode::tag>*>(static_cast<const bimap_node_sentinel*>(
+        static_cast<const node_sentinel<tag>*>(ptr_)));
     }
-
-    pointer operator->() const {
-        return &static_cast<Node const *>(ptr_)->data;
-    }
-
-    bimap_iterator &operator++() {
-        ptr_ = ptr_->next();
-        return *this;
-    }
-
-    bimap_iterator operator++(int) {
-        bimap_iterator tmp = *this;
-        ++*this;
-        return tmp;
-    }
-
-    bimap_iterator &operator--() {
-        ptr_ = ptr_->prev();
-        return *this;
-    }
-
-    bimap_iterator operator--(int) {
-        bimap_iterator tmp = *this;
-        --*this;
-        return tmp;
-    }
-
-    bool operator==(const bimap_iterator &other) const {
-        return ptr_ == other.ptr_;
-    }
-
-    bool operator!=(const bimap_iterator &other) const {
-        return !(*this == other);
-    }
-
-    bimap_iterator<BimapNode, FlipNode, Node> flip() const {
-        if (is_valid_node(ptr_)) {
-            return static_cast<const FlipNode *>(static_cast<const BimapNode *>(static_cast<const Node *>(ptr_)));
-        } else {
-            return static_cast<const node_sentinel<typename FlipNode::tag> *>(static_cast<const bimap_node_sentinel *>(
-                static_cast<const node_sentinel<tag> *>(ptr_)));
-        }
-    }
+  }
 
 private:
-    template<typename K, typename V, typename C1, typename C2>
-    friend class bimap;
+  template <typename K, typename V, typename C1, typename C2>
+  friend class bimap;
 
-    template<typename B, typename N, typename F>
-    friend class bimap_iterator;
+  template <typename B, typename N, typename F>
+  friend class bimap_iterator;
 
 
-    bimap_iterator(const node_base *ptr)
-        : ptr_(ptr) {
-    }
+  bimap_iterator(const node_base* ptr)
+    : ptr_(ptr) {}
 
-    const node_base *ptr() const {
-        return ptr_;
-    }
+  const node_base* ptr() const {
+    return ptr_;
+  }
 
-    node_base *ptr() {
-        return const_cast<node_base *>(ptr_);
-    }
+  node_base* ptr() {
+    return const_cast<node_base*>(ptr_);
+  }
 
 protected:
-    const node_base *ptr_;
+  const node_base* ptr_;
 };
--- src/intrusive_set.h
+++ src/intrusive_set.h
@@ -7,213 +7,210 @@
 #include <utility>
 
 
-template<typename Key, typename Compare, typename Tag>
+template <typename Key, typename Compare, typename Tag>
 class intrusive_set {
 public:
-    using node_t = node<Key, Tag>;
+  using node_t = node<Key, Tag>;
 
 public:
-    intrusive_set(node_base &sentinel, Compare &&compare = Compare()) noexcept
-        : sentinel_(sentinel),
-          compare_(std::move(compare)) {
+  intrusive_set(node_base& sentinel, Compare&& compare = Compare()) noexcept
+    : sentinel_(sentinel),
+      compare_(std::move(compare)) {}
+
+  intrusive_set(node_base& sentinel, const Compare& compare)
+    : sentinel_(sentinel),
+      compare_(compare) {}
+
+  intrusive_set(node_base& sentinel, intrusive_set&& other)
+    : sentinel_(sentinel),
+      compare_(std::move(other.compare_)) {}
+
+  ~intrusive_set() = default;
+
+  void swap(intrusive_set& other) noexcept {
+    using std::swap;
+    swap(compare_, other.compare_);
+  }
+
+  friend void swap(intrusive_set& lhs, intrusive_set& rhs) noexcept {
+    lhs.swap(rhs);
+  }
+
+  bool empty() const {
+    return !is_valid_node(top());
+  }
+
+  node_base* lower_bound(const Key& data) const {
+    if (top() == nullptr) {
+      return &sentinel_;
+    }
+    return lower_bound(const_cast<node_base*>(top()), data);
+  }
+
+  node_base* upper_bound(const Key& data) const {
+    auto res = lower_bound(data);
+    if (res == &sentinel_ || !equal(get_data(res), data)) {
+      return res;
+    }
+    return res->next();
+  }
+
+  node_t* insert(node_t* new_node) {
+    if (empty()) {
+      sentinel_.left = new_node;
+      sentinel_.link_right(new_node);
+      new_node->link_right(&sentinel_);
+      return new_node;
     }
 
-    intrusive_set(node_base &sentinel, const Compare &compare)
-        : sentinel_(sentinel),
-          compare_(compare) {
-    }
-
-    intrusive_set(node_base &sentinel, intrusive_set &&other)
-        : sentinel_(sentinel),
-          compare_(std::move(other.compare_)) {
-    }
-
-    ~intrusive_set() = default;
-
-    void swap(intrusive_set &other) noexcept {
-        using std::swap;
-        swap(compare_, other.compare_);
-    }
-
-    friend void swap(intrusive_set &lhs, intrusive_set &rhs) noexcept {
-        lhs.swap(rhs);
-    }
-
-    bool empty() const {
-        return !is_valid_node(top());
-    }
-
-    node_base *lower_bound(const Key &data) const {
-        if (top() == nullptr) {
-            return &sentinel_;
-        }
-        return lower_bound(const_cast<node_base *>(top()), data);
-    }
-
-    node_base *upper_bound(const Key &data) const {
-        auto res = lower_bound(data);
-        if (res == &sentinel_ || !equal(get_data(res), data)) {
-            return res;
-        }
-        return res->next();
-    }
-
-    node_t *insert(node_t *new_node) {
-        if (empty()) {
+    auto current = top();
+    while (true) {
+      if (compare_(get_data(new_node), get_data(current))) {
+        if (is_valid_node(current->left)) {
+          current = current->left;
+        } else {
+          current->link_left(new_node);
+          if (current == leftmost()) {
             sentinel_.left = new_node;
-            sentinel_.link_right(new_node);
-            new_node->link_right(&sentinel_);
-            return new_node;
+          }
+          return new_node;
         }
+      } else if (compare_(get_data(current), get_data(new_node))) {
+        if (is_valid_node(current->right)) {
+          current = current->right;
+        } else {
+          new_node->link_right(current->right);
+          current->link_right(new_node);
+          return new_node;
+        }
+      } else {
+        return static_cast<node_t*>(current);
+      }
+    }
+  }
 
-        auto current = top();
-        while (true) {
-            if (compare_(get_data(new_node), get_data(current))) {
-                if (is_valid_node(current->left)) {
-                    current = current->left;
-                } else {
-                    current->link_left(new_node);
-                    if (current == leftmost()) {
-                        sentinel_.left = new_node;
-                    }
-                    return new_node;
-                }
-            } else if (compare_(get_data(current), get_data(new_node))) {
-                if (is_valid_node(current->right)) {
-                    current = current->right;
-                } else {
-                    new_node->link_right(current->right);
-                    current->link_right(new_node);
-                    return new_node;
-                }
-            } else {
-                return static_cast<node_t *>(current);
-            }
-        }
+  node_base* find(const Key& key) const {
+    auto found = lower_bound(key);
+    return found != end() && equal(get_data(found), key) ? found : &sentinel_;
+  }
+
+  node_base* erase(node_base* pos) {
+    if (pos->dad == nullptr) {
+      return &sentinel_;
     }
 
-    node_base *find(const Key &key) const {
-        auto found = lower_bound(key);
-        return found != end() && equal(get_data(found), key) ? found : &sentinel_;
+    auto next = pos->next();
+    if (pos == leftmost()) {
+      sentinel_.left = next;
     }
 
-    node_base *erase(node_base *pos) {
-        if (pos->dad == nullptr) {
-          return &sentinel_;
+    if (pos->left == nullptr) {
+      if (pos->right != nullptr) {
+        pos->right->link_dad_from(pos);
+      } else {
+        if (pos->dad != &sentinel_) {
+          pos->dad->unlink_son(pos);
+        } else {
+          sentinel_.right = &sentinel_;
         }
-
-        auto next = pos->next();
-        if (pos == leftmost()) {
-            sentinel_.left = next;
-        }
-
-        if (pos->left == nullptr) {
-            if (pos->right != nullptr) {
-                pos->right->link_dad_from(pos);
-            } else {
-                if (pos->dad != &sentinel_) {
-                    pos->dad->unlink_son(pos);
-                } else {
-                    sentinel_.right = &sentinel_;
-                }
-            }
-            return next;
-        }
-        if (pos->right == nullptr) {
-            pos->left->link_dad_from(pos);
-            return next;
-        }
-
-        if (pos->left->right == nullptr) {
-            pos->left->link_right(pos->right);
-            pos->left->link_dad_from(pos);
-            return next;
-        }
-
-        auto left_rightmost = rightmost(pos->left);
-        erase(left_rightmost);
-        left_rightmost->link_left(pos->left);
-        left_rightmost->link_right(pos->right);
-        left_rightmost->link_dad_from(pos);
-        return next;
+      }
+      return next;
+    }
+    if (pos->right == nullptr) {
+      pos->left->link_dad_from(pos);
+      return next;
     }
 
-    const node_base *begin() const noexcept {
-        return is_valid_node(top()) ? leftmost() : end();
+    if (pos->left->right == nullptr) {
+      pos->left->link_right(pos->right);
+      pos->left->link_dad_from(pos);
+      return next;
     }
 
-    const node_base *end() const noexcept {
-        return &sentinel_;
-    }
+    auto left_rightmost = rightmost(pos->left);
+    erase(left_rightmost);
+    left_rightmost->link_left(pos->left);
+    left_rightmost->link_right(pos->right);
+    left_rightmost->link_dad_from(pos);
+    return next;
+  }
 
-    bool equal(const Key &a, const Key &b) const {
-        return !compare_(a, b) && !compare_(b, a);
-    }
+  const node_base* begin() const noexcept {
+    return is_valid_node(top()) ? leftmost() : end();
+  }
+
+  const node_base* end() const noexcept {
+    return &sentinel_;
+  }
+
+  bool equal(const Key& a, const Key& b) const {
+    return !compare_(a, b) && !compare_(b, a);
+  }
 
 private:
-    node_base *top() noexcept {
-        return sentinel_.right;
-    }
+  node_base* top() noexcept {
+    return sentinel_.right;
+  }
 
-    const node_base *top() const noexcept {
-        return sentinel_.right;
-    }
+  const node_base* top() const noexcept {
+    return sentinel_.right;
+  }
 
-    const node_base *leftmost() const noexcept {
-        return sentinel_.left;
-    }
+  const node_base* leftmost() const noexcept {
+    return sentinel_.left;
+  }
 
-    node_base *rightmost(node_base *current) {
-        while (is_valid_node(current->right)) {
-            current = current->right;
-        }
+  node_base* rightmost(node_base* current) {
+    while (is_valid_node(current->right)) {
+      current = current->right;
+    }
+    return current;
+  }
+
+  const Key& get_data(const node_base* node) const {
+    return static_cast<const node_t*>(node)->data;
+  }
+
+  const Key& get_data(const node_t* node) const {
+    return node->data;
+  }
+
+  node_base* lower_bound(node_base* current, const Key& data) const {
+    if (current == &sentinel_) {
+      return current;
+    }
+    if (compare_(get_data(current), data)) {
+      return current->right == nullptr ? &sentinel_ : lower_bound(current->right, data);
+    } else {
+      if (current->left == nullptr) {
         return current;
+      } else {
+        node_base* tmp = lower_bound(current->left, data);
+        return tmp == &sentinel_ ? current : tmp;
+      }
     }
+  }
 
-    const Key &get_data(const node_base *node) const {
-        return static_cast<const node_t *>(node)->data;
+  node_base* upper_bound(node_base* current, const Key& data) const {
+    if (current == &sentinel_) {
+      return current;
     }
-
-    const Key &get_data(const node_t *node) const {
-        return node->data;
+    if (compare_(get_data(current), data)) {
+      return current->right == nullptr ? &sentinel_ : upper_bound(current->right, data);
+    } else {
+      if (current->left == nullptr) {
+        return current;
+      } else {
+        node_base* tmp = lower_bound(current->left, data);
+        return tmp == &sentinel_ ? current : tmp;
+      }
     }
-
-    node_base *lower_bound(node_base *current, const Key &data) const {
-        if (current == &sentinel_) {
-            return current;
-        }
-        if (compare_(get_data(current), data)) {
-            return current->right == nullptr ? &sentinel_ : lower_bound(current->right, data);
-        } else {
-            if (current->left == nullptr) {
-                return current;
-            } else {
-                node_base *tmp = lower_bound(current->left, data);
-                return tmp == &sentinel_ ? current : tmp;
-            }
-        }
-    }
-
-    node_base *upper_bound(node_base *current, const Key &data) const {
-        if (current == &sentinel_) {
-            return current;
-        }
-        if (compare_(get_data(current), data)) {
-            return current->right == nullptr ? &sentinel_ : upper_bound(current->right, data);
-        } else {
-            if (current->left == nullptr) {
-                return current;
-            } else {
-                node_base *tmp = lower_bound(current->left, data);
-                return tmp == &sentinel_ ? current : tmp;
-            }
-        }
-    }
+  }
 
 private:
-    template<typename K, typename V, typename C1, typename C2>
-    friend class bimap;
+  template <typename K, typename V, typename C1, typename C2>
+  friend class bimap;
 
-    node_base &sentinel_;
-    [[no_unique_address]] Compare compare_;
+  node_base& sentinel_;
+  [[no_unique_address]] Compare compare_;
 };
